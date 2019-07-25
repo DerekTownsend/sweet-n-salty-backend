2534d778c04772392c3d1e2c977c719c667c40e5
Flatiron <flatiron@NYC-US16306-MBA.local>
2019-07-25 15:34:30 -0500

    all

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # User.create("Dave", )
+User.destroy_all
+
+
 User.create(name:"Dave", password:"yote", username:"whad")
