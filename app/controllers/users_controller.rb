ed8b182b62efaec2ea9b52b32dc0d202f56718b5
Jessica Smith <jsmith5@NYC-US16306-MBA.local>
2019-07-25 17:34:45 -0500

    updated serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
     users = User.all
-    render json: users
+    render json: UserSerializer(users).to_serialized_json
   end
 
   def show
     user = User.all
-    render json: users
+    render json: user
   end
 end
