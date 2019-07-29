cb6ca7f0f8617385878a220ca70ee06cea005b84
Jessica Smith <jsmith5@NYC-US16306-MBA.local>
2019-07-29 09:06:07 -0500

    Creating a snack works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, uniqueness: { case_sensitive: false }
   has_many :snacks
+
+  def authenticate(plaintext_password)
+    if BCrypt::Password.new(self.password_digest) == plaintext_password
+      self
+    else
+      false
+    end
+  end
 end
