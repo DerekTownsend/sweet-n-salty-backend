75129f8e619e39f8d0b6e966310c8d8dece60a0a
Derek Townsend <derekjavon2@gmail.com>
2019-07-30 18:23:26 -0500

    FULL CRUD

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -1,5 +1,5 @@
 class SnacksController < ApplicationController
-  skip_before_action :authorized, only: [:index]
+  skip_before_action :authorized, only: [:index, :show]
 
   def index
     snacks = Snack.all
@@ -12,15 +12,49 @@ class SnacksController < ApplicationController
   end
 
   def create
-    snack =  Snack.new(snack_params)
+    snack =  Snack.create(name: snack_params[:name], description: snack_params[:description], occasion: snack_params[:occasion], user: current_user)
+
     ingredients = snack_params[:ingredients_attributes].collect do |ingredient|
-      Ingredient.find_by(name: ingredient[:name], type_of_ingredient: ingredient[:type_of_ingredient])
+      temp = Ingredient.find_by(name: ingredient[:name], type_of_ingredient: ingredient[:type_of_ingredient])
+      if !temp
+        temp = Ingredient.create(name: ingredient[:name], type_of_ingredient: ingredient[:type_of_ingredient])
+      end
+      temp
     end
-    mixes = []
-     ingredients.each_with_index do |ingredient, index|
-      mixes << Mix.new(snack: snack, ingredient: ingredient, amount: snack_params[:mixes_attributes][index][:amount])
+
+    ingredients.each_with_index do |ingredient, index|
+       Mix.create(snack: snack, ingredient: ingredient, amount: snack_params[:mixes_attributes][index][:amount])
     end
-    render json: {message: "success"}
+
+    render json: SnackSerializer.new(snack).to_serialized_json
+  end
+
+  def update
+    snack =  Snack.find_by(id: params[:id])
+    snack.update_attributes(name: snack_params[:name], description: snack_params[:description], occasion: snack_params[:occasion])
+    ingredients = snack_params[:ingredients_attributes].collect do |ingredient|
+      temp = Ingredient.find_by(name: ingredient[:name], type_of_ingredient: ingredient[:type_of_ingredient])
+      if !temp
+        temp = Ingredient.create(name: ingredient[:name], type_of_ingredient: ingredient[:type_of_ingredient])
+      end
+      temp
+    end
+
+    ingredients.each_with_index do |ingredient, index|
+       temp = Mix.find_by(snack: snack, ingredient: ingredient, amount: snack_params[:mixes_attributes][index][:amount])
+       if !temp
+         temp = Mix.create(snack: snack, ingredient: ingredient, amount: snack_params[:mixes_attributes][index][:amount])
+       end
+       temp
+    end
+
+    render json: SnackSerializer.new(snack).to_serialized_json
+  end
+
+  def destroy
+
+    Snack.find_by(id: params[:id]).destroy
+    render json: {misson: "successful"}
   end
 
   private
--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,6 +1,6 @@
 class Snack < ApplicationRecord
   belongs_to :user
-  has_many :mixes
+  has_many :mixes, :dependent => :destroy
   has_many :ingredients, through: :mixes
   accepts_nested_attributes_for :mixes, :ingredients
 end
